cc1e2b3a64672119445c2748b5a8ab7348052723
RegantivariGowthami <rgowthamireddy277@gmail.com>
2024-04-08 21:28:13 +0530

    first commit

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,9 +3,12 @@ import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { ShopContext } from '../../context/ShopContext'
 export const Navbar = () => {
 
     const [menu,setMenu] = useState("shop");
+    const {getTotalCartItems}=useContext(ShopContext);
     
   return (
     <div className='navbar'>
@@ -22,7 +25,7 @@ export const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
     </div>
   )
