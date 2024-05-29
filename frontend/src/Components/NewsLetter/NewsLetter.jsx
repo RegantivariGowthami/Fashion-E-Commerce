8098ee370482cea8af9b102b95c45bb4259fe4b5
RegantivariGowthami <rgowthamireddy277@gmail.com>
2024-05-29 12:06:06 +0530

    Backend

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -4,7 +4,7 @@ import './NewsLetter.css'
 export const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclosive Offers On Your Email</h1>
+        <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
             <input type="email" placeholder='your Email id' />
